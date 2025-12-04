313e8758cb553ab571f50c7163670515e5209f99
07balajee <harisbalajee61@gmail.com>
2025-12-04 23:39:51 +0530

    TASK2- quiz with 3 questions.

--- classproject_DAY4/quiz.js
+++ classproject_DAY4/quiz.js
@@ -1,6 +1,30 @@
-const c = document.getElementById("const");
-const l = document.getElementById("let");
-const s = document.getElementById("static");
-const v = document.getElementById("var");
+const questc = document.getElementById("questc");
+const ques = document.getElementById("ques");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const btn = document.getElementById("next");
+let count = 1;
 
-c.addEventListner
+btn.addEventListener("click", () => {
+    count++;
+
+    if (count == 2) {
+        questc.innerHTML = "Question 2 of 3";
+        ques.innerHTML = "Which Method is used to log the console?";
+        one.innerHTML = "print()";
+        two.innerHTML = "console.log()";
+        three.innerHTML = "log()";
+        four.innerHTML = "console()";
+    }
+
+    if (count == 3) {
+        questc.innerHTML = "Question 3 of 3";
+        ques.innerHTML = "Which of these is NOT a JavaScript data type?";
+        one.innerHTML = "number";
+        two.innerHTML = "string";
+        three.innerHTML = "boolean";
+        four.innerHTML = "character";
+    }
+});
